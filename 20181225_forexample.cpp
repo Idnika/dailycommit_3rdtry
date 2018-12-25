264e636ada165d15e23307e6297414b077e6a2f3
Jaehee Choi <sizna35@gmail.com>
2018-12-25 23:33:26 +0900

    for example

---
+++ 20181225_forexample.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+
+  // Write a for loop here:
+  for (int i = 99; i > 0; i--)
+  {
+    std::cout << i << " bottles of pop on the wall.\n";
+    std::cout << "Take one down and pass it around.\n";
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
